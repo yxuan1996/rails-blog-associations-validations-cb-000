133f4ca544b676df46e9f9dc71272a71f5c27567
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-29 15:47:13 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ActiveRecord::Base
 
   validates :name, presence: true
   validates :content, presence: true
-  
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   has_many :post_tags
-  has_many :posts, :through => :post_tags
+  has_many :posts, through: :post_tags
 
-  validates :name, uniqueness: true
+  validates_uniqueness_of :name
 end
